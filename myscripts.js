6bda8fba8bfa63eecc611ffbbbf0d44e8b82ae4c
Thomas Archer <thoms.archer@gmail.com>
2018-10-25 13:05:23 -0700

    Test commit

---
+++ myscripts.js
@@ -0,0 +1,13 @@
+//Javascript test upload
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "yourusername",
+  password: "yourpassword"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
