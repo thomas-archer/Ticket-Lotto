b571dcd45c7ac54706a33f3ef8ef1091881ad39b
hamestoy <44349916+hamestoy@users.noreply.github.com>
2018-10-25 11:15:52 -0700

    Add files via upload

---
+++ DBInitialization.sql
@@ -0,0 +1,39 @@
+CREATE TABLE Sellers(
+	SellerID VARCHAR(5) PRIMARY KEY,
+	SellerName VARCHAR(25),
+	Email VARCHAR(25) UNIQUE,
+    Hpassword VARCHAR(25),
+	Phone VARCHAR(10));
+    
+    CREATE TABLE SellerEvents(
+	EventID VARCHAR(5) PRIMARY KEY,
+    SellerID VARCHAR(5),
+	EventDate DATE,
+    Price  DECIMAL(10,2),
+    NumTickets INT,
+	City VARCHAR(25),
+	Street VARCHAR(25),
+	Zip VARCHAR(5),
+    State VARCHAR(2),
+    EventDesc MEDIUMTEXT,
+    URL VARCHAR(2083),
+	FOREIGN KEY(SellerID) REFERENCES Sellers(SellerID));
+    
+    CREATE TABLE Buyers(
+	BuyerID VARCHAR(5) PRIMARY KEY,
+	BuyerName VARCHAR(25),
+	Email VARCHAR(25) UNIQUE,
+    Hpassword VARCHAR(25),
+    City VARCHAR(25),
+	Street VARCHAR(25),
+	Zip VARCHAR(5),
+    State VARCHAR(2),
+	Phone VARCHAR(10));
+    
+    CREATE TABLE TicketRequests(
+    BuyerID VARCHAR(5),
+    EventID VARCHAR(5),
+    TicketStatus VARCHAR(10),
+    FOREIGN KEY(BuyerID) REFERENCES Buyers(BuyerID),
+    FOREIGN KEY(EventID) REFERENCES SellerEvents(EventID),
+    PRIMARY KEY (BuyerID, EventID));
