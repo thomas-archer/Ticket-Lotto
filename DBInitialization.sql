bfc387284ac80c2cd86788289d5c426700178428
hamestoy <44349916+hamestoy@users.noreply.github.com>
2018-10-26 08:33:46 -0700

    Update DBInitialization.sql

--- DBInitialization.sql
+++ DBInitialization.sql
@@ -2,38 +2,38 @@ CREATE TABLE Sellers(
 	SellerID VARCHAR(5) PRIMARY KEY,
 	SellerName VARCHAR(25),
 	Email VARCHAR(25) UNIQUE,
-    Hpassword VARCHAR(25),
+    	Hpassword VARCHAR(25),
 	Phone VARCHAR(10));
-    
-    CREATE TABLE SellerEvents(
+		
+CREATE TABLE SellerEvents(
 	EventID VARCHAR(5) PRIMARY KEY,
-    SellerID VARCHAR(5),
+        SellerID VARCHAR(5),
 	EventDate DATE,
-    Price  DECIMAL(10,2),
-    NumTickets INT,
+    	Price  DECIMAL(10,2),
+    	NumTickets INT,
 	City VARCHAR(25),
 	Street VARCHAR(25),
 	Zip VARCHAR(5),
-    State VARCHAR(2),
-    EventDesc MEDIUMTEXT,
-    URL VARCHAR(2083),
+    	State VARCHAR(2),
+    	EventDesc MEDIUMTEXT,
+    	URL VARCHAR(2083),
 	FOREIGN KEY(SellerID) REFERENCES Sellers(SellerID));
     
-    CREATE TABLE Buyers(
+CREATE TABLE Buyers(
 	BuyerID VARCHAR(5) PRIMARY KEY,
 	BuyerName VARCHAR(25),
 	Email VARCHAR(25) UNIQUE,
-    Hpassword VARCHAR(25),
-    City VARCHAR(25),
+    	Hpassword VARCHAR(25),
+    	City VARCHAR(25),
 	Street VARCHAR(25),
 	Zip VARCHAR(5),
-    State VARCHAR(2),
+    	State VARCHAR(2),
 	Phone VARCHAR(10));
     
-    CREATE TABLE TicketRequests(
-    BuyerID VARCHAR(5),
-    EventID VARCHAR(5),
-    TicketStatus VARCHAR(10),
-    FOREIGN KEY(BuyerID) REFERENCES Buyers(BuyerID),
-    FOREIGN KEY(EventID) REFERENCES SellerEvents(EventID),
-    PRIMARY KEY (BuyerID, EventID));
+CREATE TABLE TicketRequests(
+    	BuyerID VARCHAR(5),
+    	EventID VARCHAR(5),
+    	TicketStatus VARCHAR(10),
+    	FOREIGN KEY(BuyerID) REFERENCES Buyers(BuyerID),
+    	FOREIGN KEY(EventID) REFERENCES SellerEvents(EventID),
+    	PRIMARY KEY (BuyerID, EventID));
