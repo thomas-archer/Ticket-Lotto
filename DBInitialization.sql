553925699d7ce651ca2045fab274beac3ebe9b8c
Thomas Archer <thoms.archer@gmail.com>
2018-10-30 18:23:21 -0700

    Organized files, made create-event php, and organized database structure to match

--- DBInitialization.sql
+++ DBInitialization.sql
@@ -1,24 +1,24 @@
 CREATE TABLE SellerEvents(
 	EventID VARCHAR(5) PRIMARY KEY,
+	EventName VARCHAR(25),
+	EventURL VARCHAR(2083),
 	EventDate DATE,
-	TicketDistDate DATE
-    	Price  DECIMAL(10,2),
-    	NumTickets INT,
-	City VARCHAR(25),
-	Street VARCHAR(25),
-	Zip VARCHAR(5),
-    	State VARCHAR(2),
-    	EventDesc MEDIUMTEXT,
-    	URL VARCHAR(2083),
-    	SellerName VARCHAR(25),
-    	SellerEmail VARCHAR(25));
+	TicketDistDate DATE,
+	Price  DECIMAL(10,2),
+	NumTickets INT,
+	-- City VARCHAR(25), Exclude address for now?
+	-- Street VARCHAR(25),
+	-- Zip VARCHAR(5),
+	-- 	State VARCHAR(2),
+	EventDesc MEDIUMTEXT,
+	SellerEmail VARCHAR(25));
     
 	
 CREATE TABLE TicketRequests(
-    	EventID VARCHAR(5),
-    	TicketStatus VARCHAR(10),
-    	BuyerName VARCHAR(25),
+    EventID VARCHAR(5),
+    TicketStatus VARCHAR(10),
+    BuyerName VARCHAR(25),
 	BuyerEmail VARCHAR(25) 
-    	FOREIGN KEY(EventID) REFERENCES SellerEvents(EventID),
+    FOREIGN KEY(EventID) REFERENCES SellerEvents(EventID),
 	PRIMARY KEY(EventID, BuyerEmail));
     
---
+++ join-lotto.php
@@ -0,0 +1,33 @@
+<?php
+//File that takes form info on event page and inserts info into database
+if(isset($_POST['submitted'])) {
+  include('connect-mysql.php');
+  $name = $_POST['name_input'];
+  $email = $_POST['email_input'];
+  $sql = "INSERT INTO TicketRequests (BuyerName, BuyerEmail) VALUES ('name', 'email')";
+}
+
+if ($dbcon->query($sql) === TRUE) {
+  echo "Submission Successful!";
+} else {
+  echo "Error: " . $sql . "<br>" . $dbcon->error;
+}
+?>
+
+<!-- account craetion not yet implemented <?php 
+//
+// File that takes form info on buyer account creation page and inserts info into database
+// if(isset($_POST['submitted'])) {
+//   include('connect-mysql.php');
+//   $name = $_POST['name'];
+//   $email = $_POST['email'];
+//   $psword = $_POST['psword'];
+//   $sql = "INSERT INTO Buyers (BuyerName, Email, Hpasswrod) VALUES ('name', 'email', 'psword')";
+// }
+
+// if ($dbcon->query($sql) === TRUE) {
+//   echo "Submission Successful!";
+// } else {
+//   echo "Error: " . $sql . "<br>" . $dbcon->error;
+// }
+?> -->
