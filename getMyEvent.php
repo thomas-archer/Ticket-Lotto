7530b74244fca3956f34d5baa271ce6f92ff9078
rmacdonald01 <43978218+rmacdonald01@users.noreply.github.com>
2018-11-05 21:36:20 -0800

    Function to grab events associated with an email
    it's it fam!

---
+++ getMyEvent.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+
+include(connect-mysql.php);
+
+$mysql_select_db(DB_NAME) or die("FUck you");
+$query2 = "SELECT EventName, EventDate FROM SellerEvents WHERE EventID in (SELECT EventID FROM TicketRequests WHERE BuyerEmail = 'bobbymac97@gmail.com')";
+
+$result = $dbcon->query($query2);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br> Event Name: ". $row["EventName"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+?>
+
+</body>
+</html>
