faae3d343d98d3b409087a29e10425f78ec28d7f
Thomas Archer <thoms.archer@gmail.com>
2018-11-06 10:02:05 -0800

    Updated connection info

--- connect-mysql.php
+++ connect-mysql.php
@@ -1,7 +1,7 @@
 <?php
 //File that connects to MySQL database
 $servername = "ticketdb.cadri9on7p25.us-east-1.rds.amazonaws.com";
-$username = "username";
+$username = "ticketlotto";
 $password = "ticketlotto";
 $dbname = "ticketlotto";
 
@@ -10,4 +10,4 @@ $dbcon = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($dbcon->connect_error) {
     die("Connection failed: " . $dbcon->connect_error);
-} 
+}
