7cdfbc5d6f01e776dc23c64d52b41a867c2405a5
hamestoy <44349916+hamestoy@users.noreply.github.com>
2018-11-27 14:55:18 -0800

    Add files via upload

--- functions.php
+++ functions.php
@@ -1,6 +1,51 @@
 
 <?php
+    //for use on home page
+    function getSearchedEvents($EventName){
+        $servername = "ticketdb.cadri9on7p25.us-east-1.rds.amazonaws.com";
+        $username = "ticketlotto";
+        $password = "ticketlotto";
+        $dbname = "ticketlotto";
+        // Create connection
+            $dbcon = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($dbcon->connect_error) {
+                    die("Connection failed: " . $dbcon->connect_error);
+            }
+            $query = "Select EventID,EventName,EventDate,TicketDistDate,Price from SellerEvents where EventName = \"$EventName\"";
+            $result = $dbcon->query($query);
+            $Events = array();
+            while($row = $result->fetch_row())
+            {
+                $Events[] = $row;
+            }
+    $result->close();
+    $mysqli->close();
+    return $Events;
+    }
 
+    //for use on home page
+    function insertTicketRequest($EventID,$BuyerName,$BuyerEmail){
+        $servername = "ticketdb.cadri9on7p25.us-east-1.rds.amazonaws.com";
+        $username = "ticketlotto";
+        $password = "ticketlotto";
+        $dbname = "ticketlotto";
+        // Create connection
+            $dbcon = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($dbcon->connect_error) {
+                    die("Connection failed: " . $dbcon->connect_error);
+            }
+            $query = "INSERT INTO TicketRequests VALUES ($EventID,\"waiting\",\"$BuyerName\",\"$BuyerEmail\")";
+            if ($dbcon->query($query) === TRUE) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $dbcon->error;
+            }
+            $dbcon->close();
+    }
+
+    //for use on View My Events Page
 	function getSellerEvents($SellerEmail){
             $servername = "ticketdb.cadri9on7p25.us-east-1.rds.amazonaws.com";
             $username = "ticketlotto";
@@ -20,10 +65,40 @@
                 	$Events[] = $row;
                 }
 		$result->close();
-		$mysqli->close();
+		$dbcon->close();
         return $Events;
     }
 
+    //for use on View My Events Page
+    function distributeTickets($EventID)
+    {
+
+    }
+
+    //For use on create event page
+    function insertNewEvent($EventName,$Organizer,$URL,$EventDate,$TicketDistDate,$Price,$NumTickets,$EventDesc,$SellerEmail)
+    {
+        $servername = "ticketdb.cadri9on7p25.us-east-1.rds.amazonaws.com";
+        $username = "ticketlotto";
+        $password = "ticketlotto";
+        $dbname = "ticketlotto";
+        // Create connection
+            $dbcon = new mysqli($servername, $username, $password, $dbname);
+            // Check connection
+            if ($dbcon->connect_error) {
+                    die("Connection failed: " . $dbcon->connect_error);
+            }
+        $query = "INSERT INTO SellerEvents (EventName, EventOrganization, EventURL, EventDate, TicketDistDate, Price, NumTickets, EventDesc, SellerName, SellerEmail) 
+        VALUES (\"$EventName\", \"$Organizer\", \"$URL\", \"$EventDate\", \"$TicketDistDate\", $Price, $NumTickets, \"$EventDesc\", \"$SellerEmail\")";
+        if ($dbcon->query($query) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $dbcon->error;
+        }
+        $dbcon->close();
+    }
+
+    
     function getTicketRequestsEvent($EventID){
         $servername = "ticketdb.cadri9on7p25.us-east-1.rds.amazonaws.com";
         $username = "ticketlotto";
@@ -43,11 +118,12 @@
             $TicketRequest[] = $row;
         }
         $result->close();
-        $mysqli->close();
+        $dbcon->close();
         return $TicketRequest;
     }
 
-    function getBuyerEvents($BuyerEmail){
+    //for use on ticket verification page
+    function getBuyerEventsDist($BuyerEmail){
         $servername = "ticketdb.cadri9on7p25.us-east-1.rds.amazonaws.com";
         $username = "ticketlotto";
         $password = "ticketlotto";
@@ -58,8 +134,8 @@
         if ($dbcon->connect_error) {
                 die("Connection failed: " . $dbcon->connect_error);
         }
-        $query = "Select * from SellerEvents where EventID in
-                (Select distinct EventID from TicketRequests where BuyerEmail = \"$BuyerEmail\")";
+        $query = "Select EventID, EventName from SellerEvents where EventID in
+                (Select distinct EventID from TicketRequests where BuyerEmail = \"$BuyerEmail\" and TicketStatus = \"distributed\")";
         $result = $dbcon->query($query);
         $BuyerEvents = array();
         while($row = $result->fetch_row())
@@ -69,5 +145,54 @@
         $result->close();
         $mysqli->close();
         return $BuyerEvents;
+    }
+
+    //for use on ticket verification page
+    function updateStatus($EventID,$BuyerEmail,$TicketStatus)
+    {
+        $servername = "ticketdb.cadri9on7p25.us-east-1.rds.amazonaws.com";
+        $username = "ticketlotto";
+        $password = "ticketlotto";
+        $dbname = "ticketlotto";
+        // Create connection
+        $dbcon = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($dbcon->connect_error) {
+                die("Connection failed: " . $dbcon->connect_error);
+        }
+        $query = "Update TicketRequests Set status = $TicketStatus Where EventID = $EventID and BuyerEmail =\"$BuyerEmail\"";
+        if ($dbcon->query($query) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $dbcon->error;
+        }
+        $dbcon->close();
+
+    }
+
+    //for use on ticket verification page
+    function getURL($EventID)
+    {
+        $servername = "ticketdb.cadri9on7p25.us-east-1.rds.amazonaws.com";
+        $username = "ticketlotto";
+        $password = "ticketlotto";
+        $dbname = "ticketlotto";
+        // Create connection
+        $dbcon = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($dbcon->connect_error) {
+                die("Connection failed: " . $dbcon->connect_error);
+        }
+        $query = "Select EventURL from SellerEvents where EventID = $EventID";
+        $result = $dbcon->query($query);
+        $URL = array();
+        while($row = $result->fetch_row())
+        {
+            $URL[] = $row;
+        }
+        $result->close();
+        $dbcon->close();
+        return $URL;
+
     }
 ?>
