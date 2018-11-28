c81c604342626e0d1c2fde5532a424759a738872
rmacdonald01 <43978218+rmacdonald01@users.noreply.github.com>
2018-11-27 20:37:48 -0800

    Table View + Search Functions

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+	include 'functions.php';
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -9,55 +13,59 @@
 			<ul id = "menu">
 				<li><a href="index.php">Home</a></li>
 				<li><a href="create.php">Create Event</a></li>
-				<li><a href="viewEvents.php">View My Events</a></li>
+				<li><a href="viewEvents.php">Manage My Events</a></li>
 			</ul>
-			<h1 align="center">- The Ticket Lottery -</h1>
-			<div align="center" class="copyright">
-				<input type="text" placeholder="Search">
+			<h1 align="center">The Ticket Lottery</h1>
+			<div align="center" class="homeSearch">
+				<div class="searchBar">
+					<input name="Event" class="search-txt" type="text" placeholder="Search by Event Name">
+				</div>
 			</div>
 
-			<div class="gallery">
-				<div class="thumbnail"> <a href="eventDetails.html"><img src="images/bkg_06.jpg" alt="" width="2000" class="cards"/></a>
-					<h4>TITLE</h4>
-					<p class="tag">HTML, CSS, JS, WordPress</p>
-					<p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+			<form action='' method="POST">
+				<table id='eventList'>
+						<tr>
+							<th>Event Name</th>
+							<th>Event Date</th>
+							<th>Price</th>
+							<th>Distribution Date</th>
+							<th>Select</th>
+						</tr>
+
+						<?php
+
+							$result = getSearchedEvents($Event);
+
+							if ($result->num_rows > 0) {
+							    // output data of each row
+							    while($row = $result->fetch_assoc()) {
+							        echo "<tr>
+							        		<td>". $row["EventName"]. "</td>
+							        		<td>". $row["EventDate"]. "</td>
+							        		<td>". $row["Price"]. "</td>
+							        		<td>". $row["TicketDistDate"]. "</td>
+							        		<td> <input type='radio' name='selection' value=". $row["EventID"]. "> Select </td>
+							        	  </tr>";
+							    }
+							} else {
+							    echo "0 results";
+							}
+						?>
+				</table>
+				<br>
+				<div class="eventSubmit">
+					Name:
+					<input type="text" name="name">
+					Email:
+					<input type="text" name="email">
+					<br><br>
+					<input type="submit" value="Submit">
 				</div>
-				<div class="thumbnail"> <a href="#"><img src="images/bkg_06.jpg" alt="" width="2000" class="cards"/></a>
-					<h4>TITLE</h4>
-					<p class="tag">HTML, CSS, JS, WordPress</p>
-					<p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				</div>
-				<div class="thumbnail"> <a href="#"><img src="images/bkg_06.jpg" alt="" width="2000" class="cards"/></a>
-					<h4>TITLE</h4>
-					<p class="tag">HTML, CSS, JS, WordPress</p>
-					<p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				</div>
-				<div class="thumbnail"> <a href="#"><img src="images/bkg_06.jpg" alt="" width="2000" class="cards"/></a>
-					<h4>TITLE</h4>
-					<p class="tag">HTML, CSS, JS, WordPress</p>
-					<p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				</div>
-				<div class="thumbnail"> <a href="#"><img src="images/bkg_06.jpg" alt="" width="2000" class="cards"/></a>
-					<h4>TITLE</h4>
-					<p class="tag">HTML, CSS, JS, WordPress</p>
-					<p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				</div>
-				<div class="thumbnail"> <a href="#"><img src="images/bkg_06.jpg" alt="" width="2000" class="cards"/></a>
-					<h4>TITLE</h4>
-					<p class="tag">HTML, CSS, JS, WordPress</p>
-					<p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				</div>
-				<div class="thumbnail"> <a href="#"><img src="images/bkg_06.jpg" alt="" width="2000" class="cards"/></a>
-					<h4>TITLE</h4>
-					<p class="tag">HTML, CSS, JS, WordPress</p>
-					<p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				</div>
-				<div class="thumbnail"> <a href="#"><img src="images/bkg_06.jpg" alt="" width="2000" class="cards"/></a>
-					<h4>TITLE</h4>
-					<p class="tag">HTML, CSS, JS, WordPress</p>
-					<p class="text_column">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-				</div>
-			</div>
-  		</div>
+			</form>
+
+			<?php
+
+			?>		
+		</div>
 	</body>
 </html>
